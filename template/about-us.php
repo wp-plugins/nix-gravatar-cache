c9f11013f66586493f3abe566def78651cd0d371
nixsolutions <nixsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-07 14:27:42 +0000

    add about-us.php
    git-svn-id: https://plugins.svn.wordpress.org/nix-gravatar-cache/trunk@724008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ template/about-us.php
@@ -0,0 +1,11 @@
+<div class="postbox-container">
+	<div  class="postbox ">
+		<h3  class="hndle"><?php _e('Contact Us', 'nix'); ?></h3>
+		<div class="nix-antispam-about-us inside">
+           <b class="phone_logo">Phone:</b> +38 057 784-06-00<br>
+           <b>Fax:</b> +38 057 784-06-16<br>
+           <b>Site:</b> <a href="http://nixsolutions.com/departments/cms/">nixsolutions.com/departments/cms/</a><br>
+           <b>E-mail:</b> <a title="Mail To cms_pm@nixsolutions.com" href="mailto:cms_pm@nixsolutions.com">cms_pm@nixsolutions.com</a>
+		</div>
+	</div>
+</div>
